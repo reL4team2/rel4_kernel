d5285f99a4c0aa658c5d847fbf586df793c212ee
yfblock <321353225@qq.com>
2024-07-06 03:38:56 +0800

    Fix mi_dev_zy Panic and Rusty ArchTCB (#31)
    * try to sort the arch_init_freemem
    
    * update the cache code
    
    * try to change the common registers
    
    * fix the prev bug
    
    * add something in aarch64 platform
    
    * try to fix the prev bug
    
    * try to change the register use in sel4_task
    
    * FIX: Fix Bug And rusty
    
    * FIX: Format Code
    
    * CI: Test New Result Parse
    
    * FIX: fix bug
    
    * fmt: Rusty Reg partial Code
    
    * CI: Fix parse
    
    * CI: Add new parse framework
    
    * CI: Fix Python SHEBANG
    
    ---------
    
    Co-authored-by: ZhiyuanSue <2262387848@qq.com>

--- src/kernel/fastpath.rs
+++ src/kernel/fastpath.rs
@@ -112,11 +112,8 @@ pub fn fastpath_mi_check(msgInfo: usize) -> bool {
 #[inline]
 #[no_mangle]
 pub fn fastpath_copy_mrs(length: usize, src: &mut tcb_t, dest: &mut tcb_t) {
-    let mut reg: usize;
-    for i in 0..length {
-        reg = msgRegister[0] + i;
-        dest.set_register(reg, src.get_register(reg));
-    }
+    dest.tcbArch
+        .copy_range(&src.tcbArch, msgRegister[0]..msgRegister[0] + length);
 }
 
 // #[inline]
