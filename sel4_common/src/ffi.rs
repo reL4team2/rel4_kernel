71fe0c18789e14536f8111400416f35094759660
yufeng <321353225@qq.com>
2024-07-09 17:31:44 +0800

    Move Rust crates outside

--- sel4_common/src/ffi.rs
+++
@@ -1,9 +0,0 @@
-extern "C" {
-    pub fn kernel_stack_alloc();
-}
-
-#[cfg(feature = "ENABLE_SMP")]
-/// This function is used to map the core.
-extern "C" {
-    pub fn coreMap();
-}
--- sel4_task/src/deps.rs
+++
@@ -1,5 +0,0 @@
-extern "C" {
-    pub fn ksIdleThreadTCB();
-    #[cfg(feature = "ENABLE_SMP")]
-    pub fn doMaskReschedule(mask: usize);
-}
