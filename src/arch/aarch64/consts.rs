971a4f1c27aa369713bdd7f03f607c11cbcb335a
ZhiQiu-Ovo <lilonghao23@mails.ucas.ac.cn>
2024-07-10 14:02:43 +0800

    implement map_it_pt_cap

--- src/arch/aarch64/consts.rs
+++ src/arch/aarch64/consts.rs
@@ -1,6 +1,6 @@
 use sel4_common::MASK;
 
 pub const PPTR_TOP: usize = 0xFFFFFFFF80000000;
-pub const physBase: usize = 0x80000000;
+pub const physBase: usize = 0x4000_0000;
 pub const KERNEL_ELF_PADDR_BASE: usize = physBase + 0x4000000;
 pub const KERNEL_ELF_BASE: usize = PPTR_TOP + (KERNEL_ELF_PADDR_BASE & MASK!(30));
--- src/boot/utils.rs
+++ src/boot/utils.rs
@@ -92,21 +92,6 @@ pub fn provide_cap(root_cnode_cap: &cap_t, cap: cap_t) -> bool {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn map_it_pt_cap(_vspace_cap: &cap_t, _pt_cap: &cap_t) {
-    let vptr = _pt_cap.get_pt_mapped_address();
-    let lvl1pt = convert_to_mut_type_ref::<pte_t>(_vspace_cap.get_cap_ptr());
-    let pt = _pt_cap.get_cap_ptr();
-    let pt_ret = lvl1pt.lookup_pt_slot(vptr);
-    #[cfg(target_arch = "riscv64")]
-    {
-        let targetSlot = convert_to_mut_type_ref::<pte_t>(pt_ret.ptSlot as usize);
-        *targetSlot = pte_t::new(pptr_to_paddr(pt) >> seL4_PageBits, PTEFlags::V);
-        sfence();
-    }
-    #[cfg(target_arch = "aarch64")]
-    todo!();
-}
 
 pub fn create_it_pt_cap(vspace_cap: &cap_t, pptr: pptr_t, vptr: vptr_t, asid: usize) -> cap_t {
     let cap = cap_t::new_page_table_cap(asid, pptr, 1, vptr);
